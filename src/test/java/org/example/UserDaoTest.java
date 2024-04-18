f9dce6b5f1646d176b684824b58c280c15f45dbc
chp320 <chp320@naver.com>
2024-04-18 23:56:43 +0900

    간단한 jdbc 프로그래밍 구현

---
+++ src/test/java/org/example/UserDaoTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserDaoTest {
+
+    /* 테스트 코드 수행 전 실행 필요한 경우를 위해 작성 */
+    @BeforeEach
+    void setUp() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("db_schema.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
+    @Test
+    void createTest() throws SQLException {
+        UserDao userDao = new UserDao();
+
+        userDao.create(new User("wizard", "password", "name", "email"));        // DB 에 저장
+
+        User user = userDao.findByUserId("wizard");     // DB 에서 데이터 조회
+        assertThat(user).isEqualTo(new User("wizard", "password", "name", "email"));
+    }
+}
