7bd0c9af34c358ee18de8c76194803d302d6f48c
chp320 <chp320@naver.com>
2024-04-19 22:17:00 +0900

    UserDao 에서 처리하던 커넥션 연결 기능 (getConnection메서드)을 ConnectionManager 로 이관 + 리팩토링

--- src/main/java/org/example/ConnectionManager.java
+++ src/main/java/org/example/ConnectionManager.java
@@ -3,15 +3,41 @@ package org.example;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionManager {
-    public static DataSource getDataSource() {
+    private static final String DB_DRIVER = "org.h2.Driver";
+    public static final String DB_URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    public static final int MAX_POOL_SIZE = 40;
+
+    private static final DataSource ds;
+
+    // DataSource 는 하나여야 하기 때문에 static 으로 초기화 수행!!
+    static {
         HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDriverClassName("org.h2.Driver");       // resources 디렉토리 하위에 생성
-        hikariDataSource.setJdbcUrl("jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1");
+        hikariDataSource.setDriverClassName(DB_DRIVER);       // resources 디렉토리 하위에 생성
+        hikariDataSource.setJdbcUrl(DB_URL);
         hikariDataSource.setUsername("sa");
         hikariDataSource.setPassword("");
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE);     // 커넥션 수 설정
+        hikariDataSource.setMinimumIdle(MAX_POOL_SIZE);
 
-        return hikariDataSource;
+        ds = hikariDataSource;
     }
+
+    // Connection 을 얻어오는 메서드
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static DataSource getDataSource() {
+        return ds;
+    }
+
 }
